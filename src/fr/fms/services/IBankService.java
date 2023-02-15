1fe0d31fd110bbfbf392379ed3c214ba5df8a865
Ayyoub MEHDIOUI <MehdiouiA@ad.fms-ea.com>
2023-02-15 15:31:43 +0100

    add interfaceBankService

--- src/fr/fms/services/IBankService.java
+++ src/fr/fms/services/IBankService.java
@@ -1,5 +1,20 @@
 package fr.fms.services;
 
+import fr.fms.entities.Account;
+
 public interface IBankService {
+// interface ServiceBank 
+
+	// fourni methode consulter compte {id}
+	Account getAccount(int id);
+
+	// methode versement
+	void makeWithdrawal(int accountId, double amount);
+
+	// methode retrait
+	void makeDeposit(int accountId, double amount);
+
+	// methode virement
+	void makeTransfer(int accountId_withdrawal, int accountId_deposit, double amount);
 
 }
--- src/fr/fms/tests/Test.java
+++ src/fr/fms/tests/Test.java
@@ -1,8 +1,16 @@
 package fr.fms.tests;
-import fr.fms.entities.*;
+
 public class Test {
 
 	public static void main(String[] args) {
+		// creer un menu BankServices // gérer idealement via un switch case
+		// 1 : Consulter Mon compte
+		// 2 : Verser des sous
+		// 3 : Retirer des sous
+		// 4 : Virement
+
+		// pour l implement de IBankServices construct() => pour acceder au services
+		// (showAccountDetails(id) _ deposit(id ,amount) _ withdrawal(id,amount)
 
 	}
 }
