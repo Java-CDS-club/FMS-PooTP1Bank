ac66640948c0d1fccd3bbcde53bae8145eaa0c80
Samuel LE PORCHER <Le-porcherS@ad.fms-ea.com>
2023-02-16 15:06:04 +0100

    addAccount

---
+++ src/fr/fms/services/IBankAccountImpl.java
@@ -0,0 +1,28 @@
+package fr.fms.services;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import fr.fms.entities.Account;
+
+public class IBankAccountImpl implements IBankAccount {
+
+	public Map<Integer, Account> accounts;
+	
+	public IBankAccountImpl() {
+		accounts = new HashMap<>();
+	}
+	
+	@Override
+	public void addAccount(Account account) {
+		
+		accounts.put(account.getId(), account);
+	}
+
+	@Override
+	public List<Account> findCustomerAccount(int idCustomer) {
+		return null;
+	}
+
+}
