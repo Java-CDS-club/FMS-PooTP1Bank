9da436b663bd53f9b5360f93043541b7cc0ab477
Francois Supervielle-Brouques <francoisup40@gmail.com>
2023-02-16 09:47:10 +0100

    add IBankServiceImpl

---
+++ src/fr/fms/services/IBankServiceImpl.java
@@ -0,0 +1,31 @@
+package fr.fms.services;
+
+import fr.fms.entities.Account;
+
+public class IBankServiceImpl implements IBankService {
+
+	@Override
+	public Account getAccount(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void makeWithdrawal(int accountId, double amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void makeDeposit(int accountId, double amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void makeTransfer(int accountId_withdrawal, int accountId_deposit, double amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
