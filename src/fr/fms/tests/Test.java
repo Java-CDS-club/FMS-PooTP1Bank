9ab4f4946b05239bf17fbf3e9d7563e3bc6301ef
Camille GASCHET <camille.gaschet1984@gmail.com>
2023-02-16 15:52:28 +0100

    Add full menu

--- src/fr/fms/tests/Test.java
+++ src/fr/fms/tests/Test.java
@@ -1,22 +1,88 @@
 package fr.fms.tests;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// creer un menu BankServices // gérer idealement via un switch case
-		// 1 : Consulter Mon compte
-		// 2 : Verser des sous
-		// 3 : Retirer des sous
-		// 4 : Virement
-		// 5 : ADMIN
-		// 6 : Quitter l 'application
 
-		// acce a un menu admin demander un mdp => (*** codePushers***)
+		System.out.println("Bienvenue chez FMS myBank™. Que souhaitez-vous faire ?");
 
-		// Bienvenue dans le menu ADMIN
-		// 1 : Ajouter un client
-		// 2 : Ajouter un compte
-		// 3 : Retour au menu pricipal
+		int input = 0;
+		Scanner scan = new Scanner(System.in);
 
-	}
+		// Affichage du menu principal
+		while (input < 6) {
+			System.out.println();
+			System.out.println("1 : Consulter mon compte");
+			System.out.println("2 : Effectuer un versement");
+			System.out.println("3 : Effectuer un retrait");
+			System.out.println("4 : Effectuer un virement");
+			System.out.println("5 : ADMIN");
+			System.out.println("6 : Quitter l'application");
+			System.out.println();
+			while (!scan.hasNextInt()) scan.next();
+			input = scan.nextInt();	
+			switch(input) {
+			// Consulter mon compte
+			case 1:
+				System.out.println("Consulter mon compte");
+
+				break;
+			// Effectuer un versement
+			case 2:
+				System.out.println("Effectuer un versement");
+
+				break;
+			// Effectuer un retrait
+			case 3:
+				System.out.println("Effectuer un retrait");
+
+				break;
+			// Effectuer un virement
+			case 4:
+				System.out.println("Effectuer un virement");
+
+				break;
+			// ADMIN
+			case 5:
+				System.out.println("ADMIN");
+				input = 0;
+				while (input < 3) {
+					System.out.println();
+					System.out.println("1 : Ajouter un client");
+					System.out.println("2 : Ajouter un compte");
+					System.out.println("3 : Retourner au menu principal");
+					System.out.println();
+					while (!scan.hasNextInt()) scan.next();
+					input = scan.nextInt();
+					switch(input) {
+					// Ajouter un client
+					case 1:
+						System.out.println("Ajouter un client");
+
+						break;
+					// Ajouter un compte
+					case 2:
+						System.out.println("Ajouter un compte");
+
+						break;
+					// Retourner au menu principal
+					case 3:
+						System.out.println("Retourner au menu principal");
+
+						break;
+					default : System.out.println("Erreur de saisie");
+					}
+				}
+				break;
+			// Quitter l'application
+			case 6:
+				System.out.println("Quitter l'application");
+
+				break;
+			default : System.out.println("Erreur de saisie");
+			}
+		}
+	}	
 }
